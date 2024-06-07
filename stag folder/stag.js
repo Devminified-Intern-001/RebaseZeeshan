967def96e84089ebba6a522f7754d29631ef0061
MuhammadZeeshan-Zee <155021919+MuhammadZeeshan-Zee@users.noreply.github.com>
2024-06-07 12:12:47 +0500

    satg is commit initially

---
+++ stag folder/stag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function stag() {
+  return (
+    <div>
+      <p>1st commit</p>
+      <p>2nd commit</p>
+      <p>3rd commit</p>
+    </div>
+  );
+}
+
+export default stag;
